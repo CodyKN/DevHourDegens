510eda4011afcb95f6a499f272aabcc61daa5f0b
Cody Nguyen <36778689+CodyKN@users.noreply.github.com>
2023-09-29 19:04:19 -0500

    Change homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ Index.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLayout>;
 export default function Index() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dev Hour Degens
+      Nick can dev hour deez nuts
     </main>
   )
 }
